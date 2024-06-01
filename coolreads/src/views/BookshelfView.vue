728d1aa1e231f991ecacd3ffcc3ec9d14090a5f8
joaocasr <castrojoao839@gmail.com>
2024-06-01 23:28:10 +0100

    paginacao e filtragem da prateleira

--- coolreads/src/views/BookshelfView.vue
+++ coolreads/src/views/BookshelfView.vue
@@ -8,7 +8,7 @@ import AddBookSearchComponent from '../components/AddBookSearchComponent.vue';
     <div class="books-page">
         <div id="searchbox" v-if="showSearchBox==true"><AddBookSearchComponent @close="showSearchBox=false"/></div>
         <div class="text-wrapper-4">My Bookshelf</div> 
-        <input class="search-bookshelf" type="text" placeholder="Filter by book name"/> 
+        <input v-model="search_input" class="search-bookshelf" type="text" placeholder="Filter by book name"/> 
         <div class="div">
             <div class="ibooks-template">
                 <div class="overlap">
@@ -16,13 +16,24 @@ import AddBookSearchComponent from '../components/AddBookSearchComponent.vue';
                     <div class="book-row">
                         <div class="book">
                             <div class="overlap-group">
-                                <div v-for="book in bookshelf" v-if="!book">
+                                <div v-for="book in displayBooksPerPage" v-if="!book">
                                     <BookComponent :cover="book.cover" ></BookComponent>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+        <div class="pagination">
+            <div class="pagination-child">
+            </div>
+            <div class="div">
+            <div class="parent">
+            <img class="vector-icon" @click="backPage()" alt="" src="/img/back.svg">
+            <div v-for="(n,index) in nrpages" class="div3" :class="activate[n -1]==true? 'child':''">{{n}}</div>
+            <img class="vector-icon1" @click="nextPage()" alt="" src="/img/front.svg">
+            </div>
             </div>
         </div>
     </div>
@@ -36,23 +47,86 @@ export default {
     data(){
         return {
                 bookshelf:[
-                { cover: "/img/book.png", id: 1 },
-                { cover: "/img/book.png", id: 1 },
-                { cover: "/img/book.png", id: 1 },
-                { cover: "/img/book.png", id: 1 },
-                { cover: "/img/book.png", id: 1 },
-                { cover: "/img/book.png", id: 1 },
-                { cover: "/img/book.png", id: 1 },
-                { cover: "/img/book.png", id: 1 }
+                { titulo:"biografia", cover: "/img/biografia.png", id: 1 },
+                { titulo:"desporto", cover: "/img/desporto.png", id: 2 },
+                { titulo:"misterio", cover: "/img/misterio.png", id: 3 },
+                { titulo:"musica",cover: "/img/musica.png", id: 4 },
+                { titulo:"biografia", cover: "/img/biografia.png", id: 1 },
+                { titulo:"desporto", cover: "/img/desporto.png", id: 2 },
+                { titulo:"misterio", cover: "/img/misterio.png", id: 3 },
+                { titulo:"musica",cover: "/img/musica.png", id: 4 },
+                { titulo:"biografia", cover: "/img/biografia.png", id: 1 },
+                { titulo:"desporto", cover: "/img/desporto.png", id: 2 },
+                { titulo:"misterio", cover: "/img/misterio.png", id: 3 },
+                { titulo:"musica",cover: "/img/musica.png", id: 4 },
+                { titulo:"biografia", cover: "/img/biografia.png", id: 1 },
+                { titulo:"desporto", cover: "/img/desporto.png", id: 2 },
+                { titulo:"misterio", cover: "/img/misterio.png", id: 3 },
+                { titulo:"musica",cover: "/img/musica.png", id: 4 },
+                { titulo:"biografia", cover: "/img/biografia.png", id: 1 },
+                { titulo:"desporto", cover: "/img/desporto.png", id: 2 },
+                { titulo:"misterio", cover: "/img/misterio.png", id: 3 },
+                { titulo:"musica",cover: "/img/musica.png", id: 4 },
+                { titulo:"biografia", cover: "/img/biografia.png", id: 1 },
+                { titulo:"desporto", cover: "/img/desporto.png", id: 2 },
+                { titulo:"misterio", cover: "/img/misterio.png", id: 3 },
+                { titulo:"musica",cover: "/img/musica.png", id: 4 },
+                { titulo:"biografia", cover: "/img/biografia.png", id: 1 },
+                { titulo:"desporto", cover: "/img/desporto.png", id: 2 },
+                { titulo:"misterio", cover: "/img/misterio.png", id: 3 },
+                { titulo:"musica",cover: "/img/musica.png", id: 4 },
+                { titulo:"biografia", cover: "/img/biografia.png", id: 1 },
+                { titulo:"desporto", cover: "/img/desporto.png", id: 2 },
+                { titulo:"misterio", cover: "/img/misterio.png", id: 3 },
+                { titulo:"musica",cover: "/img/musica.png", id: 4 },
+                { titulo:"fantasia",cover: "/img/fantasia.png", id: 5 }
             ],
-            showSearchBox:false
+            activate : [true,false],
+            showSearchBox:false,
+            page: 0,
+            maxPerPage : 30,
+            nrpages : 0,
+            search_input: '',
+            filtered: []
         }
     },
     methods: {
         changeSearchBoxVisibility(){
             this.showSearchBox = !this.showSearchBox
             console.log("estado:"+this.showSearchBox)
-
+        },
+        nextPage(){
+            if(this.page+1<=this.bookshelf.length){
+                this.activate[this.page] = !this.activate[this.page]
+                this.page = this.page + 1;
+                this.activate[this.page] = !this.activate[this.page]
+            }
+        },
+        backPage(){
+            if(this.page-1>=0){
+                this.activate[this.page] = !this.activate[this.page]
+                this.page = this.page - 1;
+                this.activate[this.page] = !this.activate[this.page]
+            }
+        }
+    },
+    computed: {
+        displayBooksPerPage(){
+            if (this.search_input !== '') {
+                this.filtered = this.bookshelf.filter(b => b.titulo.toLowerCase().includes(this.search_input))
+            }else{
+                this.filtered = this.bookshelf
+            }
+            const startIndex = this.maxPerPage * this.page;
+            const endIndex = startIndex + this.maxPerPage;
+            this.nrpages = Math.ceil(this.filtered.length / this.maxPerPage);
+            console.log(startIndex)
+            console.log(endIndex)
+            if(startIndex>=0 && startIndex<this.filtered.length && endIndex>0 && endIndex<this.filtered.length) return this.filtered.slice(startIndex, endIndex);
+            else if(endIndex>=this.filtered.length) {
+                const endIndex=this.filtered.length;
+                return this.filtered.slice(startIndex, endIndex);
+            }else return this.filtered;
         }
     }
 }
@@ -273,7 +347,7 @@ export default {
     position: absolute;
     width: 739px;
     height: 57px;
-    top: 334px;
+    top: 444px;
     left: 115px;
     background-color: #e6e6e66e;
     border-radius: 25px;
@@ -300,6 +374,78 @@ export default {
     line-height: normal;
 } 
 
+.pagination-child {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    box-shadow: 0px 4.972476959228516px 4.97px rgba(0, 0, 0, 0.2);
+    border-radius: 32.32px;
+    background-color: #f7f9ff;
+    width: 542px;
+    height: 68.4px;
+}
+.vector-icon {
+    position: absolute;
+    width: 30px;
+    left: -18%;
+    overflow: hidden;
+    object-fit: contain;
+    height: 30px;
+}
+.vector-icon:hover {
+    cursor:pointer;
+}
+.vector-icon1 {
+    width: 30px;
+    position: relative;
+    height: 30px;
+}
+.vector-icon1:hover {
+    cursor:pointer;
+}
+
+.child {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    border-radius: 50%;
+    background-color: #8798d4;
+    width: 62px;
+    height: 62px;
+}
+.div3 {
+    width: 32px;
+    position: relative;
+    font-weight: 600;
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    height: auto;
+    flex-shrink: 0;
+}
+.parent {
+    position: absolute;
+    top: 20px;
+    left: 90px;
+    width: 363px;
+    height: 19.5px;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: flex-start;
+    gap: 26px;
+    color: #656565;
+    font-size: 20px;
+}
+.pagination {
+    width: 100%;
+    position: relative;
+    height: 68.4px;
+    text-align: center;
+    font-size: 18px;
+    color: #fff;
+    font-family: Lato;
+} 
 .overlap-group {
     display: grid;
     grid-template-columns: auto auto auto auto auto auto;
