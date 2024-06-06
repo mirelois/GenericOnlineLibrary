fca3dc9dac5f853a2372d44405a2d899501ff62f
hiddenduck <cmachado0032@gmail.com>
2024-06-06 19:12:01 +0100

    Update BookshelfController.java

--- backend/coolreads/src/main/java/com/aa/coolreads/User/controllers/BookshelfController.java
+++ backend/coolreads/src/main/java/com/aa/coolreads/User/controllers/BookshelfController.java
@@ -1,9 +1,18 @@
 package com.aa.coolreads.User.controllers;
 
-import com.aa.coolreads.User.dto.BookShelfDTO;
+import com.aa.coolreads.User.dto.BookShelfCreationDTO;
+import com.aa.coolreads.User.dto.PersonalBookDTO;
+import com.aa.coolreads.User.exception.BookshelfNotFoundException;
+import com.aa.coolreads.User.exception.CustomerNotFoundException;
+import com.aa.coolreads.User.models.Customer;
 import com.aa.coolreads.User.services.BookshelfService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping(value = "/customer/{username}/bookshelf")
@@ -17,7 +26,21 @@ public class BookshelfController {
     }
 
     @PostMapping
-    public void insertBookshelf(@PathVariable String username, @RequestBody BookShelfDTO bookshelf){
+    public ResponseEntity<Void> insertBookshelf(@PathVariable String username, @RequestBody BookShelfCreationDTO bookshelf){
+        try {
+            this.bookshelfService.insertBookshelf(bookshelf, username);
+            return ResponseEntity.ok().build();
+        } catch (CustomerNotFoundException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
 
+    @GetMapping("/{name}")
+    public ResponseEntity<Set<PersonalBookDTO>> getBooks(@PathVariable String username, @PathVariable String name, @RequestParam Integer page, @RequestParam Integer size){
+        try{
+            return ResponseEntity.ok().body(this.bookshelfService.getBooks(name, username, page, size));
+        } catch (BookshelfNotFoundException | CustomerNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
     }
 }
