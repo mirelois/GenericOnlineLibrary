8361f8f850d915b581f428d6feda5e5368291a7e
unknown <jmbarbosa2002@gmail.com>
2024-06-07 18:47:14 +0100

    Stylin', profilin', limousine riding

--- coolreads/src/router/index.js
+++ coolreads/src/router/index.js
@@ -4,6 +4,7 @@ import BookshelfView from '../views/BookshelfView.vue'
 import BookDetailView from '../views/BookDetailView.vue'
 import BooksMenuView from '../views/BooksMenuView.vue'
 import SignupPage from '@/views/SignupPage.vue'
+import ProfilePageView from '@/views/ProfilePageView.vue'
 import LoginPage from '@/views/LoginPage.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -35,6 +36,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfilePageView
     },
     {
       path: '/login',
