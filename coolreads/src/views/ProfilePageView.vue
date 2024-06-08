51611ae655e08a3891917a925e81c5cb281fb72c
José Miguel Barbosa <79376027+zeisalone@users.noreply.github.com>
2024-06-08 14:03:36 +0100

    Less Bloat + mini correção da font do profile

--- coolreads/src/views/ProfilePageView.vue
+++ coolreads/src/views/ProfilePageView.vue
@@ -66,7 +66,9 @@ import FooterComponent from '../components/FooterComponent.vue';
     <div class="view-more-wrapper">
         <div class="view-more">View More</div>
     </div>
-        
+	<div class="posFoot"><FooterComponent></FooterComponent></div>
+		
+	
 <NavComponent></NavComponent>
 </template>
 
@@ -75,210 +77,7 @@ import FooterComponent from '../components/FooterComponent.vue';
 @import url('https://fonts.googleapis.com/css2?family=Inika:wght@400&display=swap');
 
 
-body {
-  	margin: 0;
-  	line-height: normal;
-}
-.popup-overlay {
-  	display: flex;
-  	flex-direction: column;
-  	position: fixed;
-  	inset: 0;
-}
 
-.c {
-  	position: relative;
-}
-.logo {
-  	position: absolute;
-  	top: 0px;
-  	left: 33px;
-  	height: 115px;
-  	display: flex;
-  	flex-direction: column;
-  	align-items: center;
-  	justify-content: center;
-  	padding: 10px;
-  	box-sizing: border-box;
-  	font-size: 96px;
-  	color: #c48930;
-}
-.vuesaxoutlineframe-icon {
-  	width: 31.7px;
-  	position: relative;
-  	height: 31.7px;
-}
-.chevron-icon {
-  	width: 34px;
-  	position: relative;
-  	height: 17px;
-  	opacity: 0.8;
-}
-.profile-section {
-  	position: absolute;
-  	top: 0px;
-  	left: 1032px;
-  	width: 184px;
-  	height: 115px;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: center;
-  	justify-content: center;
-  	gap: 11px;
-}
-.settings-icon {
-  	width: 31px;
-  	position: relative;
-  	height: 31px;
-  	overflow: hidden;
-  	flex-shrink: 0;
-}
-.settings-section {
-  	position: absolute;
-  	top: 0px;
-  	left: 1216px;
-  	width: 159px;
-  	height: 115px;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: center;
-  	justify-content: center;
-  	gap: 11px;
-}
-.vuesaxoutlinehome-2-icon {
-  	width: 32.3px;
-  	position: relative;
-  	height: 32.3px;
-}
-.home-section {
-  	position: absolute;
-  	top: 0px;
-  	left: 141px;
-  	width: 144px;
-  	height: 115px;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: center;
-  	justify-content: center;
-  	gap: 17px;
-}
-.search-icon {
-  	position: absolute;
-  	top: 20px;
-  	left: 20px;
-  	width: 30px;
-  	height: 30px;
-}
-.search-for-books {
-  	position: absolute;
-  	top: 22px;
-  	left: 62px;
-}
-.search-box {
-  	position: absolute;
-  	top: 22px;
-  	left: 462px;
-  	border-radius: 15px;
-  	background-color: #6b6b6b;
-  	width: 556px;
-  	height: 70px;
-  	overflow: hidden;
-  	font-size: 20px;
-  	color: #2f3134;
-}
-.book-light-icon {
-  	width: 34px;
-  	position: relative;
-  	height: 34px;
-}
-.books-section {
-  	position: absolute;
-  	top: -1px;
-  	left: 285px;
-  	width: 142px;
-  	height: 115px;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: center;
-  	justify-content: center;
-  	padding: 41px 7px;
-  	box-sizing: border-box;
-  	gap: 15px;
-}
-.bell-light-icon {
-  	width: 33px;
-  	position: relative;
-  	height: 33px;
-}
-.notifications-section {
-  	position: absolute;
-  	top: 0px;
-  	left: 1375px;
-  	width: 235px;
-  	height: 115px;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: center;
-  	justify-content: center;
-  	gap: 8px;
-}
-.icon {
-  	width: 36px;
-  	position: relative;
-  	height: 33px;
-  	overflow: hidden;
-  	flex-shrink: 0;
-}
-.bookshelf-section {
-  	position: absolute;
-  	top: -1px;
-  	left: 1610px;
-  	width: 209px;
-  	height: 115px;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: center;
-  	justify-content: center;
-  	gap: 8px;
-}
-.navegador {
-  	position: absolute;
-  	top: 0px;
-  	left: 0px;
-  	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  	background-color: #000;
-  	width: 1819px;
-  	height: 115px;
-  	overflow: hidden;
-}
-.icon1 {
-  	position: absolute;
-  	height: 99.79%;
-  	width: 100%;
-  	top: 0%;
-  	right: 0%;
-  	bottom: 0.21%;
-  	left: 0%;
-  	max-width: 100%;
-  	overflow: hidden;
-  	max-height: 100%;
-  	object-fit: cover;
-  	mix-blend-mode: saturation;
-}
-.icon2 {
-  	position: absolute;
-  	height: 99.79%;
-  	width: 100%;
-  	top: 0.21%;
-  	right: 0%;
-  	bottom: 0%;
-  	left: 0%;
-  	max-width: 100%;
-  	overflow: hidden;
-  	max-height: 100%;
-  	object-fit: cover;
-  	mix-blend-mode: overlay;
-}
 .parent {
   	position: absolute;
   	height: 423.77%;
@@ -287,19 +86,6 @@ body {
   	right: -2.85%;
   	bottom: -161.77%;
   	left: -2.86%;
-}
-.cherry-bomb-1 {
-  	position: absolute;
-  	height: 112.01%;
-  	width: 36.86%;
-  	top: -6.29%;
-  	right: 8.43%;
-  	bottom: -5.72%;
-  	left: 54.71%;
-  	max-width: 100%;
-  	overflow: hidden;
-  	max-height: 100%;
-  	object-fit: cover;
 }
 .frame {
   	position: absolute;
@@ -322,6 +108,7 @@ body {
 .about-me {
   	position: relative;
   	line-height: 25px;
+	font-size: 40px;
 }
 .container {
   	align-self: stretch;
@@ -539,63 +326,12 @@ body {
   	overflow: hidden;
   	font-size: 25px;
 }
-.background {
-  	position: absolute;
-  	top: 0px;
-  	left: -3px;
-  	background-color: #6b6b6b;
-  	width: 1824px;
-  	height: 151px;
-}
-.linearity-gmb {
-  	position: absolute;
-  	width: 100%;
-  	top: calc(50% - 10.55px);
-  	left: 0%;
-  	line-height: 26px;
-  	display: inline-block;
-  	height: 21.1px;
-  	mix-blend-mode: normal;
-}
-.footer-ui1 {
-  	position: absolute;
-  	width: 100%;
-  	top: calc(50% + 40.89px);
-  	right: 0%;
-  	left: 0%;
-  	height: 21.1px;
-}
-.c1 {
-  	position: absolute;
-  	top: 0px;
-  	left: 108px;
-  	font-size: 96px;
-  	font-family: Inika;
-  	color: #c48930;
-  	text-align: left;
-  	display: inline-block;
-  	width: 57px;
-  	height: 101.5px;
-}
-.footer-ui {
-  	position: absolute;
-  	width: 16.2%;
-  	top: calc(50% - 62px);
-  	right: 40.69%;
-  	left: 43.11%;
-  	height: 124px;
-}
-.footer {
-  	position: absolute;
-  	top: 3259px;
+.posFoot{
+	position: absolute;
+	top: 3259px;
   	left: 1px;
-  	background-color: #fff;
-  	width: 1821px;
+  	width: 671px;
   	height: 124px;
-  	overflow: hidden;
-  	text-align: center;
-  	font-size: 16px;
-  	font-family: Inter;
 }
 .view-more {
   	position: absolute;
@@ -614,65 +350,6 @@ body {
   	font-size: 25px;
   	color: #000;
 }
-.profilepage {
-  	width: 100%;
-  	position: relative;
-  	background-color: #222831;
-  	height: 3383px;
-  	overflow: hidden;
-  	text-align: left;
-  	font-size: 24px;
-  	color: #fff;
-  	font-family: Inika;
-}
-
-
-.view-profile {
-  	position: relative;
-}
-.viewprofileoption {
-  	position: absolute;
-  	top: 0px;
-  	left: 0px;
-  	background-color: rgba(219, 160, 70, 0.85);
-  	border: 1px solid #fff;
-  	box-sizing: border-box;
-  	width: 184px;
-  	height: 60px;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: center;
-  	justify-content: center;
-  	cursor: pointer;
-}
-.signoutoption {
-  	position: absolute;
-  	top: 64px;
-  	left: 0px;
-  	background-color: rgba(219, 160, 70, 0.85);
-  	border: 1px solid #fff;
-  	box-sizing: border-box;
-  	width: 184px;
-  	height: 60px;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: center;
-  	justify-content: center;
-  	cursor: pointer;
-}
-.dropdown-profile {
-  	width: 184px;
-  	position: relative;
-  	height: 124px;
-  	max-width: 90%;
-  	max-height: 90%;
-  	overflow: auto;
-  	text-align: left;
-  	font-size: 24px;
-  	color: #fff;
-  	font-family: Inika;
-}
-
 
 
 </style>
