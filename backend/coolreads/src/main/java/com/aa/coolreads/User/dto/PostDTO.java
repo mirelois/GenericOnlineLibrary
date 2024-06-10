66e32e4fb00bad1c0ab249d7f246cee0cd43e719
mirelois <79458822+mirelois@users.noreply.github.com>
2024-06-10 21:13:21 +0100

    Added PostDTO

---
+++ backend/coolreads/src/main/java/com/aa/coolreads/User/dto/PostDTO.java
@@ -0,0 +1,31 @@
+package com.aa.coolreads.User.dto;
+
+public class PostDTO {
+
+    private String title;
+
+    private String content;
+
+    public PostDTO() {}
+
+    public PostDTO(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
