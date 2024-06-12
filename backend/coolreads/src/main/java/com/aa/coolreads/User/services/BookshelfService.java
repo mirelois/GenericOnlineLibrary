68c97aa3972da014b158412b172e8b05b9896942
hiddenduck <cmachado0032@gmail.com>
2024-06-12 15:24:36 +0100

    cache

--- backend/coolreads/src/main/java/com/aa/coolreads/User/services/BookshelfService.java
+++ backend/coolreads/src/main/java/com/aa/coolreads/User/services/BookshelfService.java
@@ -17,6 +17,9 @@ import com.aa.coolreads.User.repositories.BookshelfRepository;
 import com.aa.coolreads.User.repositories.CustomerRepository;
 import com.aa.coolreads.User.repositories.PersonalBooksRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
@@ -45,6 +48,7 @@ public class BookshelfService {
         this.bookshelfMapper = bookshelfMapper;
     }
 
+    @Cacheable(value = "bookshelf", key = "#username")
     @Transactional
     public Set<SimpleBookShelfDTO> getBookshelf(String username) throws CustomerNotFoundException {
 
@@ -53,6 +57,7 @@ public class BookshelfService {
         return this.bookshelfRepository.findBookshelvesByCustomer(customer).stream().map(bookshelf -> this.bookshelfMapper.toSimpleBookShelfDTO(bookshelf, this.personalBooksRepository.getBooksSize(bookshelf))).collect(Collectors.toSet());
     }
 
+    @CachePut(value = "bookshelf", key = "#username")
     @Transactional
     public void insertBookshelf(BookShelfCreationDTO bookShelfCreationDTO, String username) throws IllegalArgumentException, CustomerNotFoundException {
         Privacy privacy = Privacy.valueOf(bookShelfCreationDTO.getPrivacy().toUpperCase());
@@ -62,6 +67,7 @@ public class BookshelfService {
         this.bookshelfRepository.save(this.bookshelfMapper.toBookshelf(bookShelfCreationDTO.getName(), privacy, customer));
     }
 
+    @CacheEvict(value = "bookshelf", key = "#username")
     @Transactional
     public void updateBookshelf(String name, BookShelfCreationDTO bookShelfCreationDTO, String username) throws IllegalArgumentException, CustomerNotFoundException, BookshelfNotFoundException {
         Customer customer = this.customerRepository.findById(username).orElseThrow(() -> new CustomerNotFoundException(username));
@@ -80,6 +86,7 @@ public class BookshelfService {
         this.bookshelfRepository.save(bookshelf);
     }
 
+    @Cacheable(value = "books", key = "#username + '-' + #name")
     @Transactional
     public Set<PersonalBookDTO> getBooks(String name, String username) throws CustomerNotFoundException, BookshelfNotFoundException {
         Customer customer = this.customerRepository.findById(username).orElseThrow(() -> new CustomerNotFoundException(username));
@@ -89,6 +96,7 @@ public class BookshelfService {
         return this.personalBooksRepository.findBooks(bookshelf).stream().map(this.bookshelfMapper::toPersonalBookDTO).collect(Collectors.toSet());
     }
 
+    @CachePut(value = "books", key = "#username + '-' + #name")
     @Transactional
     public void insertBook(String name, String username, PersonalBookDTO personalBookDTO) throws CustomerNotFoundException, BookshelfNotFoundException, BookNotFoundException {
         Customer customer = this.customerRepository.findById(username).orElseThrow(() -> new CustomerNotFoundException(username));
@@ -102,6 +110,7 @@ public class BookshelfService {
         this.personalBooksRepository.save(personalBook);
     }
 
+    @CacheEvict(value = "books", key = "#username + '-' + #name")
     @Transactional
     public void deleteBook(String name, String username, String isbn) throws CustomerNotFoundException, BookshelfNotFoundException {
         Customer customer = this.customerRepository.findById(username).orElseThrow(() -> new CustomerNotFoundException(username));
