33937cf7ba92a28729fe4cd039d3cec2965b791e
hiddenduck <cmachado0032@gmail.com>
2024-06-12 16:22:46 +0100

    tags

--- backend/coolreads/src/main/java/com/aa/coolreads/User/repositories/CustomerRepository.java
+++ backend/coolreads/src/main/java/com/aa/coolreads/User/repositories/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.aa.coolreads.User.repositories;
 
 import com.aa.coolreads.User.models.Customer;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends CrudRepository<Customer, String> {
 }
