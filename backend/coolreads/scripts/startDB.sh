0753b5e368bf0ce81ce02871219b7f5e511643b6
mirelois <79458822+mirelois@users.noreply.github.com>
2024-06-17 22:48:37 +0100

    take my seed

--- backend/coolreads/scripts/startDB.sh
+++ backend/coolreads/scripts/startDB.sh
@@ -1,7 +1,7 @@
 docker run --name postgres -e POSTGRES_PASSWORD=postgres \
 -p 5432:5432 -d postgres:16
 
-sleep 10
+sleep 5
 
 docker exec -it postgres psql -U postgres -c "CREATE DATABASE cool" 
 
@@ -13,4 +13,27 @@ docker exec -it postgres psql -U postgres -d cool -c "CREATE TABLE IF NOT EXISTS
     id SERIAL PRIMARY KEY,
     ageClass VARCHAR(255) UNIQUE NOT NULL
 );"
+
+sleep 5
+
+docker exec -it postgres psql -U postgres -d cool -c "INSERT INTO Genre (genre_type) VALUES
+('scifi'),
+('fantasy'),
+('adventure'),
+('mystery'),
+('biography'),
+('manga'),
+('music'),
+('sports'),
+('romance')
+ON CONFLICT (genre_type) DO NOTHING;";
+
+
+docker exec -it postgres psql -U postgres -d cool -c "INSERT INTO AgeRange (ageClass) VALUES
+('child'),
+('teen'),
+('young_adult'),
+('adult'),
+('elder')
+ON CONFLICT (ageClass) DO NOTHING;";
 
