987938e0705c0fd17964b4d729a5790a577c2f93
unknown <jmbarbosa2002@gmail.com>
2024-06-18 00:56:30 +0100

    Notifs Fetching mas is it really?

--- coolreads/src/views/NotificationsView.vue
+++ coolreads/src/views/NotificationsView.vue
@@ -1,87 +1,117 @@
 <script setup>
 import { useStore } from 'vuex';
-import { computed } from 'vue';
+import { computed, ref, onMounted } from 'vue';
+import NavComponent from '@/components/NavComponent.vue';
 
 const store = useStore();
-
+const notifications = computed(() => store.state.notifications.notifications);
 const translations = computed(() => store.getters['language/currentTranslations']);
-const setLanguage = (language) => store.dispatch('language/setLanguage', language);
-const selectedLanguage = computed(() => store.state.language.selectedLanguage);
+const username = ref('');
+const page = ref(0);
+const maxPerPage = ref(10);
+const activate = ref([]);
 
-if (localStorage.getItem('selectedLanguage')) {
-    setLanguage(localStorage.getItem('selectedLanguage'));
-}
-</script>
+const fetchNotifications = () => {
+  store.dispatch('notifications/fetchNotifications', {
+    username: username.value,
+    pageNumber: page.value,
+    pageSize: maxPerPage.value,
+  });
+  console.log('Fetched');
+};
 
-<template>
-      <div>
-        <div class="notificationspage-child"></div>
-        <b class="all-notifications">{{ translations.allnotifications }}</b>
-        <div class="notification2all">
-          <div v-for="notification in notifications" :key="notification.id" class="notification">
-            <img class="clock-icon" alt="" src="/img/clock.svg">
-            <b class="time">{{ notification.time }}</b>
-            <b class="message">{{ notification.message }}</b>
-            <button @click="handleDelete(notification.id)">{{ translations.delete }}</button>
-          </div>
-        </div>
-        <NavComponent></NavComponent>
-      </div>
-</template>
+const handleDelete = (notificationId) => {
+  store.dispatch('notifications/deleteNotification', notificationId)
+    .then(() => {
+      console.log('Notification deleted successfully');
+      fetchNotifications();
+    })
+    .catch((error) => {
+      console.error('Error deleting notification:', error);
+    });
+};
 
+const nextPage = () => {
+  if (page.value + 1 < nrPages.value) {
+    activate.value[page.value] = false;
+    page.value += 1;
+    activate.value[page.value] = true;
+  }
+  fetchNotifications();
+};
 
-<script>
-import { mapState } from 'vuex';
-import NavComponent from '../components/NavComponent.vue';
-import FooterComponent from '../components/FooterComponent.vue';
-export default {
-  components: {
-    NavComponent,
-    FooterComponent,
-  },
-  data() {
-    return {
-      username: '',
-    };
-  },
-  computed: {
-    ...mapState({
-      notifications: state => state.notifications.notifications,
-    }),
-  },
-  created() {
-    const token = localStorage.getItem('user');
-    if (!token) {
-      console.warn('User token not found in localStorage');
-      return;
-    }
+const backPage = () => {
+  if (page.value > 0) {
+    activate.value[page.value] = false;
+    page.value -= 1;
+    activate.value[page.value] = true;
+  }
+  fetchNotifications();
+};
 
+const goToPage = (pageIndex) => {
+  activate.value[page.value] = false;
+  page.value = pageIndex;
+  activate.value[page.value] = true;
+  fetchNotifications();
+};
+
+const initializeActivate = () => {
+  activate.value = new Array(nrPages.value).fill(false);
+  if (activate.value.length > 0) activate.value[0] = true;
+};
+
+const nrPages = computed(() => Math.ceil(notifications.value.length / maxPerPage.value));
+
+const paginatedNotifications = computed(() => {
+  const start = page.value * maxPerPage.value;
+  const end = start + maxPerPage.value;
+  return notifications.value.slice(start, end);
+});
+
+onMounted(() => {
+  const token = localStorage.getItem('user');
+  if (token) {
     try {
       const decodedToken = JSON.parse(token);
-      this.username = decodedToken.info.sub;
-      this.fetchNotifications();
+      username.value = decodedToken.info.sub;
+      fetchNotifications();
+      initializeActivate();
     } catch (error) {
       console.error('Error parsing user token:', error);
     }
-  },
-  methods: {
-    fetchNotifications() {
-      this.$store.dispatch('notifications/fetchNotifications', {
-        username: this.username,
-        pageNumber: 0,
-        pageSize: 10,
-      });
-    },
-    handleDelete(notificationId) {
-      this.$store.dispatch('notifications/deleteNotification', notificationId).then(() => {
-        console.log('Notification deleted successfully');
-      }).catch(error => {
-        console.error('Error deleting notification:', error);
-      });
-    },
-  },
-};
+  } else {
+    console.warn('User token not found in localStorage');
+  }
+});
 </script>
+
+<template>
+  <div>
+    <NavComponent></NavComponent>
+    <div class="notificationspage-child"></div>
+    <b class="all-notifications">{{ translations.allnotifications }}</b>
+    <div class="notification2all">
+      <div v-for="notification in paginatedNotifications" :key="notification.id" class="notification">
+        <img class="clock-icon" alt="" src="/img/clock.svg">
+        <b class="time">{{ notification.time }}</b>
+        <b class="message">{{ notification.message }}</b>
+        <button @click="handleDelete(notification.id)">{{ translations.delete }}</button>
+      </div>
+    </div>
+    <div class="pagination">
+      <div class="pagination-child">
+        <div class="parent">
+          <img class="vector-icon" @click="backPage" alt="" src="/img/back.svg">
+          <div v-for="(n, index) in nrPages" :key="index" class="div3" :class="{ 'child': activate[n - 1] }" @click="goToPage(n - 1)">{{ n }}</div>
+          <img class="vector-icon1" @click="nextPage" alt="" src="/img/front.svg">
+        </div>
+      </div>
+    </div>
+    <NavComponent></NavComponent>
+  </div>
+</template>
+
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,700&display=swap');
@@ -158,5 +188,94 @@ export default {
   font-size: 30px;
   color: #fff;
   font-family: Inika;
+}
+
+.pagination {
+  position: absolute;
+  top: 1467px;
+  left: 464px;
+  width: 642px;
+  height: 68.4px;
+  text-align: center;
+  font-size: 18px;
+  font-family: Lato;
+}
+
+.pagination-child {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  box-shadow: 0px 4.972476959228516px 4.97px rgba(0, 0, 0, 0.2);
+  border-radius: 32.32px;
+  background-color: #f7f9ff;
+  width: 762px;
+  height: 68.4px;
+}
+
+.vector-icon {
+  width: 9px;
+  position: absolute;
+  height: 16px;
+}
+
+.vector-icon1 {
+  width: 9px;
+  position: absolute;
+  height: 16px;
+}
+
+.child {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  border-radius: 50%;
+  background-color: #8798d4;
+  width: 42px;
+  height: 42px;
+}
+
+.div1 {
+  position: absolute;
+  top: 10.5px;
+  left: 10.5px;
+  font-weight: 600;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  width: 21px;
+  height: 19.5px;
+}
+
+.div3 {
+  width: 21px;
+  position: relative;
+  font-weight: 600;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  height: 19.5px;
+  flex-shrink: 0;
+}
+
+.parent {
+  position: absolute;
+  top: 25.5px;
+  left: 78px;
+  width: 363px;
+  height: 19.5px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 36px;
+  color: #656565;
+}
+
+.div {
+  position: absolute;
+  top: 13px;
+  left: 80px;
+  width: 441px;
+  height: 42px;
 }
 </style>
