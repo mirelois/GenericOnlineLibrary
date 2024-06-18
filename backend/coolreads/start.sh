7d81f0cd8ae0b148b6615e5b2e7c269632c25fcb
hiddenduck <cmachado0032@gmail.com>
2024-06-18 14:15:37 +0100

    no docker

--- backend/coolreads/start.sh
+++ backend/coolreads/start.sh
@@ -29,10 +29,9 @@ check redis
 
 docker run --name redis --net $NETWORK_NAME -p 6379:6379 -d redis
 
-check coolreading
-
 if [ -z $1 ]; then
-    docker run --name coolreading --net $NETWORK_NAME -p 8080:8080 -d coolreads
+    echo "Not running coolreads container"
 else
+    check coolreading
     docker run --name coolreading --net $NETWORK_NAME -p 8080:8080 -e PROFILE=$1 -d coolreads
 fi
